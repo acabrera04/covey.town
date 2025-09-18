19dcfe1d63ebdfcfbec3e7ac97401d8c86bffe7a
acabrera04 <acabrera04164@gmail.com>
2025-09-17 22:15:27 -0400

    Implemented join, leave, validate move, and some checkforwins

--- townService/src/town/games/QuantumTicTacToeGame.ts
+++ townService/src/town/games/QuantumTicTacToeGame.ts
@@ -3,6 +3,14 @@ import {
   QuantumTicTacToeGameState,
   QuantumTicTacToeMove,
 } from '../../types/CoveyTownSocket';
+import InvalidParametersError, {
+  GAME_FULL_MESSAGE,
+  GAME_NOT_IN_PROGRESS_MESSAGE,
+  BOARD_POSITION_NOT_EMPTY_MESSAGE,
+  MOVE_NOT_YOUR_TURN_MESSAGE,
+  PLAYER_ALREADY_IN_GAME_MESSAGE,
+  PLAYER_NOT_IN_GAME_MESSAGE,
+} from '../../lib/InvalidParametersError';
 import Game from './Game';
 import TicTacToeGame from './TicTacToeGame';
 import Player from '../../lib/Player';
@@ -27,32 +35,103 @@ export default class QuantumTicTacToeGame extends Game<
   public constructor() {
     // TODO: implement me
     super({
-        moves: [],
-        xScore: 0,
-        oScore: 0,
-        publiclyVisible: {
-            A: [],
-            B: [],
-            C: [],
-        },
-        status: 'WAITING_FOR_PLAYERS'
+      moves: [],
+      xScore: 0,
+      oScore: 0,
+      publiclyVisible: {
+        A: [
+          [false, false, false],
+          [false, false, false],
+          [false, false, false],
+        ],
+        B: [
+          [false, false, false],
+          [false, false, false],
+          [false, false, false],
+        ],
+        C: [
+          [false, false, false],
+          [false, false, false],
+          [false, false, false],
+        ],
+      },
+      status: 'WAITING_TO_START',
     });
     this._xScore = 0;
     this._oScore = 0;
     this._moveCount = 0;
     this._games = {
-        A: new TicTacToeGame(),
-        B: new TicTacToeGame(),
-        C: new TicTacToeGame(),
+      A: new TicTacToeGame(),
+      B: new TicTacToeGame(),
+      C: new TicTacToeGame(),
     };
   }
 
   protected _join(player: Player): void {
     // TODO: implement me
+    if (this.state.x === player.id || this.state.o === player.id) {
+      throw new InvalidParametersError(PLAYER_ALREADY_IN_GAME_MESSAGE);
+    }
+    if (!this.state.x) {
+      this.state = {
+        ...this.state,
+        x: player.id,
+      };
+    } else if (!this.state.o) {
+      this.state = {
+        ...this.state,
+        o: player.id,
+      };
+    } else {
+      throw new InvalidParametersError(GAME_FULL_MESSAGE);
+    }
+    this._games.A.join(player);
+    this._games.B.join(player);
+    this._games.C.join(player);
+    if (this.state.x && this.state.o) {
+      this.state = {
+        ...this.state,
+        status: 'IN_PROGRESS',
+      };
+    }
   }
 
   protected _leave(player: Player): void {
     // TODO: implement me
+    if (this.state.x !== player.id && this.state.o !== player.id) {
+      throw new InvalidParametersError(PLAYER_NOT_IN_GAME_MESSAGE);
+    }
+    this._games.A.leave(player);
+    this._games.B.leave(player);
+    this._games.C.leave(player);
+    // Handles case where the game has not started yet
+    if (this.state.o === undefined) {
+      this.state = {
+        moves: [],
+        xScore: 0,
+        oScore: 0,
+        publiclyVisible: {
+          A: [],
+          B: [],
+          C: [],
+        },
+        status: 'WAITING_TO_START',
+      };
+      return;
+    }
+    if (this.state.x === player.id) {
+      this.state = {
+        ...this.state,
+        status: 'OVER',
+        winner: this.state.o,
+      };
+    } else {
+      this.state = {
+        ...this.state,
+        status: 'OVER',
+        winner: this.state.x,
+      };
+    }
   }
 
   /**
@@ -62,6 +141,21 @@ export default class QuantumTicTacToeGame extends Game<
    */
   private _validateMove(move: GameMove<QuantumTicTacToeMove>): void {
     // TODO: implement me
+    for (const m of this.state.moves) {
+      if (m.board === move.move.board && m.col === move.move.col && m.row === move.move.row) {
+        throw new InvalidParametersError(BOARD_POSITION_NOT_EMPTY_MESSAGE);
+      }
+    }
+    // A move is only valid if it is the player's turn
+    if (move.move.gamePiece === 'X' && this.state.moves.length % 2 === 1) {
+      throw new InvalidParametersError(MOVE_NOT_YOUR_TURN_MESSAGE);
+    } else if (move.move.gamePiece === 'O' && this.state.moves.length % 2 === 0) {
+      throw new InvalidParametersError(MOVE_NOT_YOUR_TURN_MESSAGE);
+    }
+    // A move is valid only if game is in progress
+    if (this.state.status !== 'IN_PROGRESS') {
+      throw new InvalidParametersError(GAME_NOT_IN_PROGRESS_MESSAGE);
+    }
   }
 
   public applyMove(move: GameMove<QuantumTicTacToeMove>): void {
@@ -73,12 +167,65 @@ export default class QuantumTicTacToeGame extends Game<
     this._checkForGameEnding();
   }
 
+  private _checkForWinsHelper(game: TicTacToeGame): void {
+    const board = game;
+    // A game ends when there are 3 in a row
+    // Check for 3 in a row or column
+    for (let i = 0; i < 3; i++) {
+      if (board[i][0] !== '' && board[i][0] === board[i][1] && board[i][0] === board[i][2]) {
+        this.state = {
+          ...this.state,
+          status: 'OVER',
+          winner: board[i][0] === 'X' ? this.state.x : this.state.o,
+        };
+        return;
+      }
+      if (board[0][i] !== '' && board[0][i] === board[1][i] && board[0][i] === board[2][i]) {
+        this.state = {
+          ...this.state,
+          status: 'OVER',
+          winner: board[0][i] === 'X' ? this.state.x : this.state.o,
+        };
+        return;
+      }
+    }
+    // Check for 3 in a diagonal
+    if (board[0][0] !== '' && board[0][0] === board[1][1] && board[0][0] === board[2][2]) {
+      this.state = {
+        ...this.state,
+        status: 'OVER',
+        winner: board[0][0] === 'X' ? this.state.x : this.state.o,
+      };
+      return;
+    }
+    // Check for 3 in the other diagonal
+    if (board[0][2] !== '' && board[0][2] === board[1][1] && board[0][2] === board[2][0]) {
+      this.state = {
+        ...this.state,
+        status: 'OVER',
+        winner: board[0][2] === 'X' ? this.state.x : this.state.o,
+      };
+      return;
+    }
+    // Check for no more moves
+    if (this.state.moves.length === 9) {
+      this.state = {
+        ...this.state,
+        status: 'OVER',
+        winner: undefined,
+      };
+    }
+  }
+
   /**
    * Checks all three sub-games for any new three-in-a-row conditions.
    * Awards points and marks boards as "won" so they can't be played on.
    */
   private _checkForWins(): void {
     // TODO: implement me
+    this._checkForWinsHelper(this._games.A);
+    this._checkForWinsHelper(this._games.B);
+    this._checkForWinsHelper(this._games.C);
   }
 
   /**
