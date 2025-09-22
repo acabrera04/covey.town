5b349e24fc0c436ca260c3ffe755184eb84be1c2
acabrera04 <acabrera04164@gmail.com>
2025-09-21 21:45:01 -0400

    Frontend configured for Quantum Tic Tac Toe

--- frontend/src/classes/interactable/QuantumTicTacToeAreaController.ts
+++ frontend/src/classes/interactable/QuantumTicTacToeAreaController.ts
@@ -124,8 +124,44 @@ export default class QuantumTicTacToeAreaController extends GameAreaController<
   }
 
   protected _updateFrom(newModel: GameArea<QuantumTicTacToeGameState>): void {
+    const wasOurTurn = this.whoseTurn?.id === this._townController.ourPlayer.id;
     super._updateFrom(newModel);
     // TODO: implement the rest of this
+    const newState = newModel.game;
+    if (newState) {
+      const newBoards: { A: TicTacToeCell[][]; B: TicTacToeCell[][]; C: TicTacToeCell[][] } = {
+        A: [
+          [undefined, undefined, undefined],
+          [undefined, undefined, undefined],
+          [undefined, undefined, undefined],
+        ],
+        B: [
+          [undefined, undefined, undefined],
+          [undefined, undefined, undefined],
+          [undefined, undefined, undefined],
+        ],
+        C: [
+          [undefined, undefined, undefined],
+          [undefined, undefined, undefined],
+          [undefined, undefined, undefined],
+        ],
+      };
+      newState.state.moves.forEach(move => {
+        if (
+          newBoards[move.board][move.row][move.col] === undefined &&
+          (newState.state.publiclyVisible[move.board][move.row][move.col] ||
+            move.gamePiece === this.gamePiece)
+        ) {
+          newBoards[move.board][move.row][move.col] = move.gamePiece;
+        }
+      });
+      if (!_.isEqual(newBoards, this._boards)) {
+        this._boards = newBoards;
+        this.emit('boardChanged', this._boards);
+      }
+    }
+    const isOurTurn = this.whoseTurn?.id === this._townController.ourPlayer.id;
+    if (wasOurTurn != isOurTurn) this.emit('turnChanged', isOurTurn);
   }
 
   public async makeMove(
